11db7d2647c9272326d7267b36722da23d77564e
Avan <mujibsardar@gmail.com>
2017-04-23 14:24:39 -0700

    Done with displaying users and user page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,15 +1,23 @@
 class UsersController < ApplicationController
   def index
+    @users = User.all
   end
 
   def show
+    @user =   User.find(params[:id])
   end
 
   def new
-  end
+  @user = User.new
+end
 
-  def create
+def create
+  @user = User.new(user_params)
+  if @user.save
+    redirect_to users_path
+    # redirect_to new_session_path
   end
+end
 
   def edit
   end
@@ -19,4 +27,11 @@ class UsersController < ApplicationController
 
   def destroy
   end
+
+  private
+  def user_params
+    params.require(:user).permit(:name,:email,:password)
+  end
+
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,32 +1,42 @@
 Rails.application.routes.draw do
-  get 'reviews/index'
+  get 'sessions/new'
 
-  get 'reviews/show'
+  get 'sessions/create'
 
-  get 'reviews/new'
+  get 'sessions/destroy'
 
-  get 'reviews/create'
+  root 'users#index'
 
-  get 'reviews/edit'
+  get '/users/:id' => 'users#show', as: :user
+  get '/users' => 'users#index', as: :users
 
-  get 'reviews/update'
 
-  get 'reviews/destroy'
+  # get 'reviews/index'
+  #
+  # get 'reviews/show'
+  #
+  # get 'reviews/new'
+  #
+  # get 'reviews/create'
+  #
+  # get 'reviews/edit'
+  #
+  # get 'reviews/update'
+  #
+  # get 'reviews/destroy'
+  #
+  # get 'users/index'
+  #
+  # get 'users/show'
+  #
+  # get 'users/new'
+  #
+  # get 'users/create'
+  #
+  # get 'users/edit'
+  #
+  # get 'users/update'
+  #
+  # get 'users/destroy'
 
-  get 'users/index'
-
-  get 'users/show'
-
-  get 'users/new'
-
-  get 'users/create'
-
-  get 'users/edit'
-
-  get 'users/update'
-
-  get 'users/destroy'
-
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root "users#index"
 end
